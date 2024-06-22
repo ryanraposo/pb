9d1cc8fb9caa9d5b30eb330578aa6372c896a81e
ryan <raposo.ryan@gmail.com>
2024-06-22 19:58:49 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='procboss',
-    version='0.1.5',
+    version='0.1.6',
     packages=find_packages(include=['pb', 'pb.*']),
     entry_points={
         'console_scripts': [
